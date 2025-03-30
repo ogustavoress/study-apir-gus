a69ef504a44c29f72e748cdf70515d965e7a88ef
ogustavoress <ogustavoress@gmail.com>
2025-03-29 22:47:57 -0300

    Ping-pong

---
+++ src/main/java/com/github/ogustavoress/study_apir_gus/StudyApirGusApplication.java
@@ -0,0 +1,13 @@
+package com.github.ogustavoress.study_apir_gus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApirGusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApirGusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ogustavoress/study_apir_gus/StudyApirGusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ogustavoress.study_apir_gus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApirGusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
