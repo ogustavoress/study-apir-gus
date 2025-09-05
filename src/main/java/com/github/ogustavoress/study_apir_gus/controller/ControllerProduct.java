96f2c37778e89bcfdeac1f9cf7d3006253cb1f32
Gustavo Carvalho <132230583+ogustavoress@users.noreply.github.com>
2025-09-05 11:47:01 +0000

    Adicionando a classe Service

--- src/main/java/com/github/ogustavoress/study_apir_gus/controller/ControllerProduct.java
+++ src/main/java/com/github/ogustavoress/study_apir_gus/controller/ControllerProduct.java
@@ -8,24 +8,33 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.ogustavoress.study_apir_gus.service.ProductService;
+
 @RestController
 @RequestMapping("products")
-public class ControllerProduct {
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
 
     @PostMapping
     public ResponseEntity<String> create(){
+        productService.create(null);
         return ResponseEntity.status(201).body("Product created.");
     }
     @GetMapping
     public ResponseEntity<String> read(){
+        productService.getById(null);
         return ResponseEntity.status(200).body("Mango");
     }
     @PutMapping
     public ResponseEntity<String> update(){
+        productService.update(null, null);
         return ResponseEntity.status(200).body("Product updated");
     }
     @DeleteMapping
     public ResponseEntity<Void> delete(){
+        productService.delete();
         return ResponseEntity.status(204).build();
     }
 
--- src/main/java/com/github/ogustavoress/study_apir_gus/service/ServiceProduct.java
+++ src/main/java/com/github/ogustavoress/study_apir_gus/service/ServiceProduct.java
@@ -4,8 +4,24 @@ import java.util.List;
 
 import com.github.ogustavoress.study_apir_gus.model.Product;
 
-public class ServiceProduct {
+public class ProductService {
 
     private List<Product> products;
+
+    public Product create (Product product){
+        return null;
+    }
+
+    public Product getById (Long id){
+        return null;
+    }
+
+    public Product update(Long id, Product product) {
+        return null;
+    }
+
+    public void delete(Long id){
+
+    }
 
 }
